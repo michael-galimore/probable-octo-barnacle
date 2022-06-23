4730aac945649395e45bf05cb543dfefdd2bf1e1
mike <michael.galimore1@gmail.com>
2022-06-22 21:10:20 -0500

    my first number exercise

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello, Mike!");
+
+        int myFirstNumber = (13 * 2) - 13;
+        System.out.println("My first number is " + myFirstNumber + "!");
     }
 }
