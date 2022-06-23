2dcd80963b67bfb83a5b5e3e68d879e38e4d531f
mike <michael.galimore1@gmail.com>
2022-06-22 21:29:33 -0500

    my first number exercise with variables

--- src/Hello.java
+++ src/Hello.java
@@ -3,7 +3,14 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Mike!");
 
-        int myFirstNumber = (13 * 2) - 13;
-        System.out.println("My first number is " + myFirstNumber + "!");
+        int myFirstNumber = 13;
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int mySum = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(mySum);
+        int myLastOne =  1000 - mySum;
+        System.out.println(myLastOne);
+
     }
 }
+
